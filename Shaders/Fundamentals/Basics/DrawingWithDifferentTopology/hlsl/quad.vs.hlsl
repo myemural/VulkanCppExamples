7c522ca70d8db060b6ce69c55802216032951210
myemural <mustafayemural@gmail.com>
2025-09-18 18:48:33 +0300

    Added: HLSL shaders for all Basics subcategory

---
+++ Shaders/Fundamentals/Basics/DrawingWithDifferentTopology/hlsl/quad.vs.hlsl
@@ -0,0 +1,19 @@
+struct VSInput
+{
+    [[vk::location(0)]] float2 pos : POSITION;
+    [[vk::location(1)]] float3 color : COLOR0;
+};
+
+struct VSOutput
+{
+    [[vk::location(0)]] float3 Color : COLOR0;
+    float4 Position : SV_POSITION;
+};
+
+VSOutput main(VSInput input)
+{
+    VSOutput output = (VSOutput)0;
+    output.Color = input.color;
+    output.Position = float4(input.pos, 0.0, 1.0);
+    return output;
+}
