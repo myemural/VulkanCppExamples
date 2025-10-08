081461d3595b03e5a9eec293d89f76d67ee6a510
myemural <mustafayemural@gmail.com>
2025-10-09 00:34:07 +0300

    Added: HLSL shader for InstancedRendering example

---
+++ Shaders/Fundamentals/Drawing3D/InstancedRendering/hlsl/instanced_cube.vs.hlsl
@@ -0,0 +1,26 @@
+struct VSInput
+{
+    [[vk::location(0)]] float3 pos : POSITION;
+    [[vk::location(1)]] float2 uv : TEXCOORD0;
+    uint instanceID : SV_InstanceID;
+};
+
+struct UBO
+{
+    float4x4 mvp[20];
+};
+[[vk::binding(1, 0)]] ConstantBuffer<UBO> ubo;
+
+struct VSOutput
+{
+    float4 Position : SV_POSITION;
+    [[vk::location(0)]] float2 Uv : TEXCOORD0;
+};
+
+VSOutput main(VSInput input)
+{
+    VSOutput output = (VSOutput)0;
+    output.Position = mul(ubo.mvp[input.instanceID], float4(input.pos, 1.0));
+    output.Uv = input.uv;
+    return output;
+}
