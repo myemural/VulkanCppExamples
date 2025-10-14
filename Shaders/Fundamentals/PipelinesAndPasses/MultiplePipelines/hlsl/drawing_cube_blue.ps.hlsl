3fd9e978a7318bb0cf2c83b830f05178480cf2d2
myemural <mustafayemural@gmail.com>
2025-10-14 21:16:49 +0300

    Added: HLSL shaders for some of the PipelinesAndPasses examples

---
+++ Shaders/Fundamentals/PipelinesAndPasses/MultiplePipelines/hlsl/drawing_cube_blue.ps.hlsl
@@ -0,0 +1,23 @@
+// ------------------------------------------------------------------------
+// Author: Mustafa Yemural
+// Description:
+// ------------------------------------------------------------------------
+// Copyright (c) 2025 Mustafa Yemural - www.mustafayemural.com
+// Licensed under the MIT License.
+// ------------------------------------------------------------------------
+
+struct PSInput
+{
+    [[vk::location(0)]] float2 uv : TEXCOORD0;
+};
+
+[[vk::binding(0, 0)]] SamplerState uSampler;
+[[vk::binding(0, 0)]] Texture2D uImage;
+
+float4 main(PSInput input) : SV_Target
+{
+    float4 texColor = uImage.Sample(uSampler, input.uv);
+    float4 targetBlue = float4(0.0, 0.0, 1.0, texColor.a);
+    float blend = 0.5;
+    return lerp(texColor, targetBlue, blend);
+}
